18bf5fea9c695d4984a7fffc1eab60e061139dba
Stanislav Klimovich <stasklimovish@gmail.com>
2025-09-17 18:18:39 +0000

    Extended Euclidean algorithm added

--- 2.Introduction-to-git/src/euclid.py
+++ 2.Introduction-to-git/src/euclid.py
@@ -1,7 +1,25 @@
-def gcd(a, b):
+def extended_gcd(a, b):
+    x, xx = 1, 0  # коэффициенты для a
+    y, yy = 0, 1  # коэффициенты для b
+    
     while b != 0:
-        a, b = b, a % b
-    return abs(a)
+        q = a // b
+        a, b = b, a % b 
+        
+        x, xx = xx, x - q * xx
+        y, yy = yy, y - q * yy
     
-    
-assert gcd(-15, 3) == 3
+    return a, x, y # a - НОД, x y - искомые коэффициенты Безу
+
+def main():
+
+    a = int(input("Введите a: "))
+    b = int(input("Введите b: "))
+
+    gcd, x, y = extended_gcd(a, b)
+
+    print(f"\nНОД({a}, {b}) = {gcd}")
+    print(f"Коэффициенты: x = {x}, y = {y}")
+
+if __name__ == "__main__":
+    main()
