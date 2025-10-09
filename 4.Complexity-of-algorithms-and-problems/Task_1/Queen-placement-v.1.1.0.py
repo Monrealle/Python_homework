e82c71a303ca41fbc300c8e0ed2c09d6b3710d7e
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-09 19:00:48 +0000

    minor fix in v.1.1.0

--- 4.Complexity-of-algorithms-and-problems/Task_1/Queen-placement-v.1.1.0.py
+++ 4.Complexity-of-algorithms-and-problems/Task_1/Queen-placement-v.1.1.0.py
@@ -6,24 +6,24 @@ def recursive_solution(n):
         return 0
     count = 0
     array = [0] * n
-    
+
     def check(line, column):
         for i in range(line):
             if array[i] == column or abs(array[i] - column) == abs(i - line):
                 return False
         return True
-    
+
     def solve(line):
         nonlocal count
         if line == n:
             count += 1
             return
-        
+
         for column in range(n):
             if check(line, column):
                 array[line] = column
                 solve(line + 1)
-    
+
     solve(0)
     return count
 
